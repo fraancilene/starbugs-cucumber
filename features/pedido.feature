c5c25453999288321fbd9e76241aaefb0b0bc811
Francilene Silva <francilenesilva.fps10@gmail.com>
2026-02-06 10:22:22 -0300

    feat: implementando o padrão de projeto Page Object

---
+++ features/pedido.feature
@@ -0,0 +1,17 @@
+#language: pt
+
+  Funcionalidade: Pedidos
+    Como um usuário do site Starbugs, eu quero selecionar e comprar cafés
+    Para que eu possa receber os produtos em meu endereço e efetuar o pagamento na entrega
+
+    Cenario: Compra bem sucedida
+      Dado que estou na página principal da Starbugs
+        E que iniciei a compra do item "Expresso Tradicional"
+      Quando faço a busca do seguinte CEP: "72876001"
+        E informo os demais dados do endereço:
+          | number      | 206     |
+          | details     | Bloco F |
+        E escolho a forma de pagamento "Cartão de Débito"
+        E por fim finalizo a compra
+      Então sou redirecionado para a página de confirmação de pedido
+        E deve der informado um prazo de entrega entre 20 a 30 minutos
---
+++ features/step_definitions/pedido_steps.rb
@@ -0,0 +1,35 @@
+Dado('que iniciei a compra do item {string}') do |product_name|
+  @home.buy(product_name)
+end
+
+Quando('faço a busca do seguinte CEP: {string}') do |zipcode|
+  @checkout.find_zipcode(zipcode)
+end
+
+Quando('informo os demais dados do endereço:') do |table|
+  @checkout.fill_address(table.rows_hash)
+  sleep 5
+end
+
+Quando('escolho a forma de pagamento {string}') do |payment_type|
+  find('label div', text: payment_type.upcase).click
+  #//label//div[text()="Cartão de Débito"]
+
+end
+
+Quando('por fim finalizo a compra') do
+  click_on 'Confirmar pedido'
+  sleep 5
+
+end
+
+Então('sou redirecionado para a página de confirmação de pedido') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deve der informado um prazo de entrega entre {int} a {int} minutos') do |int, int2|
+  # Então('deve der informado um prazo de entrega entre {int} a {float} minutos') do |int, float|
+  # Então('deve der informado um prazo de entrega entre {float} a {int} minutos') do |float, int|
+  # Então('deve der informado um prazo de entrega entre {float} a {float} minutos') do |float, float2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
